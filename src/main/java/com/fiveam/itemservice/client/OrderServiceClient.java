6feefc6d0d34ddcf5213194504ec7b072e46dcb2
devycha <y2kdj9723@naver.com>
2023-07-04 01:58:36 +0900

    Fix: OpenFeign 로드밸런서 관련 오류 수

--- src/main/java/com/fiveam/itemservice/client/OrderServiceClient.java
+++ src/main/java/com/fiveam/itemservice/client/OrderServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("order-service")
+@FeignClient("${feign.order-service}")
 public interface OrderServiceClient {
 
     @GetMapping("/orders/item-orders/{itemOrderId}")
--- src/main/java/com/fiveam/itemservice/client/UserServiceClient.java
+++ src/main/java/com/fiveam/itemservice/client/UserServiceClient.java
@@ -4,7 +4,7 @@ import com.fiveam.itemservice.response.UserInfoResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("user-service")
+@FeignClient("${feign.user-service}")
 public interface UserServiceClient {
     @GetMapping("/users")
     UserInfoResponseDto getLoginUser();
